910ad71beb89953de3c4d9d956243f41a2e15d04
alaamerhebb <alaamerhebb@gmail.com>
2022-02-27 20:14:35 +0200

    step4.php

---
+++ php.basic1/step4.php
@@ -0,0 +1,8 @@
+<?php
+$name = 'Alaa';
+$position = "a junior web developer";
+$github_url = "https://github.com/alaamerhebb";
+echo "Hello, I'm " .$name.", I'm " .$position. " please check my github link " .$github_url. "\n";
+echo "Hello, I'm $name, I'm $position please check my github link $github_url \n";
+echo 'Hello, I\'m ' .$name.', I\'m ' .$position. ' please check my github link ' .$github_url. "\n";
+?>
