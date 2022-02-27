276a0f31a781a24f878db42629f4d19d51d4bfa2
alaamerhebb <alaamerhebb@gmail.com>
2022-02-27 23:31:26 +0200

    step5.php

---
+++ php.basic2/step5.php
@@ -0,0 +1,46 @@
+<?php
+function word_digit($word) {
+    $warr = explode(';',$word);
+    $result = '';
+    foreach($warr as $value){
+        switch(trim($value)){
+            case 'zero':
+                $result .= '0';
+                break;
+            case 'one':
+                $result .= '1';
+                break;
+            case 'two':
+                $result .= '2';
+                break;
+            case 'three':
+                $result .= '3';
+                break;
+            case 'four':
+                $result .= '4';
+                break;
+            case 'five':
+                $result .= '5';
+                break;
+            case 'six':
+                $result .= '6';
+                break;
+            case 'seven':
+                $result .= '7';
+                break;
+            case 'eight':
+                $result .= '8';
+                break;
+            case 'nine':
+                $result .= '9';
+                break;
+                default: echo "the number does not exics";
+        }
+    }
+    return $result;
+}
+
+echo word_digit("eight")."\n";
+echo word_digit("ten")."\n";
+echo word_digit("five")."\n";
+?>
