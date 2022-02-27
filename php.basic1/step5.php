7e6e86059d07e4e33741c1f438aa2a0f58f61582
alaamerhebb <alaamerhebb@gmail.com>
2022-02-27 20:51:27 +0200

    step5.php

---
+++ php.basic1/step5.php
@@ -0,0 +1,12 @@
+<?php
+$course_name = "react";
+$enrolled_students = 18;
+$price = 84.99;
+$on_discount = true;
+
+echo "Course title: $course_name  
+ Enrolled Studnets: $enrolled_students 
+ Course price: $price USD
+ Course on discount:";
+ echo $on_discount ? 'yes' : 'No';
+?>
