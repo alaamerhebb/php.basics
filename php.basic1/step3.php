d37ed84155b91c6fb251e09a312716c601669c8b
alaamerhebb <alaamerhebb@gmail.com>
2022-02-27 20:07:54 +0200

    step3.php

---
+++ php.basic1/step3.php
@@ -0,0 +1,9 @@
+<?php
+$name = "Alaa";
+$email = "alaamerhebb@gmail.com";
+echo "My name is $name and my email is $email\n";
+$height = 15;
+$width = 10;
+$area = $height * $width;
+echo "The area value is $area \n";
+?>
